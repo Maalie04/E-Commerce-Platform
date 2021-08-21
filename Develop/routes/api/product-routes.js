ec9fa7d24eeeb1ad9c8731e6d1e1db2279c7a5bb
Jamaal Lawson <maaliemaal@gmail.com>
2021-08-21 18:35:07 -0400

    createdd a new product with a post rout

--- Develop/routes/api/product-routes.js
+++ Develop/routes/api/product-routes.js
@@ -6,14 +6,18 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 // get all products
 router.get('/', (req, res) => {
   Producgt.findAll({
-    attributes: ['id', 'product_name'],
+    attributes: ['id', 'product_name','price', 'stock',],
     include: [
       {
-        model: Product,
-        attributes: ['id', 'product_name', 'price', 'stock', 'category_id']
-      }
+        model: Category,
+        attributes: ['category_name']
+      },
+      {
+        model: Tag,
+        attributes: ['tag_name']
+      },
     ]
-  }).then(categoryData => res.json(categoryData))
+  }).then(productData => res.json(productData))
   .catch(err => {
     console.log(err);
     res.status(500).json(err);
@@ -22,21 +26,40 @@ router.get('/', (req, res) => {
 
 // get one product
 router.get('/:id', (req, res) => {
-  // find a single product by its `id`
-  // be sure to include its associated Category and Tag data
+  Product.findOne({
+    where: {
+      id: req.params.id
+    },
+    attributes: ['id', 'product_name', 'price', 'stock'],
+    include: [
+      {
+        model: Category,
+        attributes: ['category_name']
+      },
+      {
+        model: Tag,
+        attributes: ['tag_name']
+      }
+    ]
+  })
+}).then(productData => {
+  if(!productData) {
+    res.status(404).json({message: 'No Product!'});
+    return;
+  }
+  res.json(productData)
+}).catch(err => {
+  res.status(500).json(err);
 });
 
 // create new product
 router.post('/', (req, res) => {
-  /* req.body should look like this...
-    {
+  Product.create({
       product_name: "Basketball",
       price: 200.00,
       stock: 3,
       tagIds: [1, 2, 3, 4]
-    }
-  */
-  Product.create(req.body)
+    })
     .then((product) => {
       // if there's product tags, we need to create pairings to bulk create in the ProductTag model
       if (req.body.tagIds.length) {
